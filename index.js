6009bb620d1d184509a8eb8b597e825c41612a54
Travis Scott <138378346+iiiMohammad@users.noreply.github.com>
2024-06-10 23:55:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Playing a game🕵🏻‍♂️ made by @i_amsneha"];
+const statusMessages = ["Playing a game🕵🏻‍♂️ made by @m12_zombie"];
 
 
 let currentIndex = 0;
